abe6aafe5b07d247b7706331686a3f05d5a56215
Ron Gierlach <rongierlach@gmail.com>
2017-11-11 21:54:04 -0600

    eth-gas-reporter config

---
+++ .ethgas.js
@@ -0,0 +1,4 @@
+module.exports = {
+  currency: "USD",    // Default: "EUR" (loaded at run-time from the `coinmarketcap` api)
+  gasPrice: 21 * 1e9  // Default: (~5 gwei, loaded at run-time from the `blockcypher` api)
+}
