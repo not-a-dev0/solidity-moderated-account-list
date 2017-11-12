153c80c54320f67ecc802c8c939fb42d807669e9
Ron Gierlach <rongierlach@gmail.com>
2017-11-11 21:54:56 -0600

    AddressList library

---
+++ contracts/AddressList.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.4.18;
+
+
+library AddressList {
+  /* Structs
+  ** ********************** */
+  struct Data {
+    address[] items;
+    mapping(address => uint) itemIdxs;
+  }
+
+  /* Public Functions
+  ** ********************** */
+  function add (Data storage self, address addrToAdd) public {
+    require(!contains(self, addrToAdd));
+    self.itemIdxs[addrToAdd] = self.items.push(addrToAdd);
+  }
+
+  function remove (Data storage self, address addrToRemove) public {
+    require(contains(self, addrToRemove));
+    uint addrToRemoveIdx = self.itemIdxs[addrToRemove] - 1;
+    address lastAddr = self.items[self.items.length - 1];
+    self.items[addrToRemoveIdx] = lastAddr;
+    delete self.items[self.items.length - 1];
+    self.itemIdxs[addrToRemove] = 0;
+    self.items.length--;
+  }
+
+  function contains (Data storage self, address addrToLookup) public view returns(bool) {
+    uint lookupIdxPlusOne = self.itemIdxs[addrToLookup];
+    if (lookupIdxPlusOne == 0) return false;
+    address foundAddr = self.items[lookupIdxPlusOne - 1];
+    return foundAddr == addrToLookup;
+  }
+}
