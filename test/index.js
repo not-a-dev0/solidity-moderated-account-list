f34cb78d62034fec4729a77e893a0bc9b61439ac
Ron Gierlach <rongierlach@gmail.com>
2017-11-14 23:42:37 -0600

    write tests

--- test/index.js
+++ test/index.js
@@ -1,25 +1,142 @@
-/*
-  test that contract seeds with initial accounts
-  test that contract seeds with initial moderators
+const { assert } = require('chai');
+const { promisifyAll } = require('bluebird');
+const AccountList = promisifyAll(artifacts.require('AccountList'));
 
-  test that owner can add an address
-  test that owner can remove an address
+contract('AccountList', (accounts) => {
+  const [
+    owner,
+    modOne, modTwo,
+    accOne, accTwo, accThree
+  ] = accounts;
+  const initialMods = [modOne, modTwo];
+  const initialAccounts = [accOne, accTwo, accThree];
 
-  test that owner can add a moderator
-  test that owner can remove a moderator
+  describe('Ownable', () => {
+    it('should have expected owner', async () => {
+      const accList = await AccountList.deployed();
+      const currOwner = await accList.owner.call();
+      assert.equal(owner, currOwner);
+    });
 
-  test that moderator can add an address
-  test that moderator can remove an address
+    it('should be able to transfer ownership', async () => {
+      const accList = await AccountList.deployed();
+      await accList.transferOwnership(modOne);
+      const currOwner = await accList.owner.call();
+      assert.equal(modOne, currOwner);
+    })
+  });
 
-  test for throw when moderator tries to add a moderator
-  test for throw when moderator tries to remove a moderator
+  describe('Moderated', () => {
+    it('should seed with initial moderators', async () => {
+      const accList = await AccountList.new([], initialMods);
+      const currMods = await accList.getModerators.call();
+      assert.sameMembers(currMods, initialMods);
+    });
 
-  test for throw when owner adds an already added address
-  test for throw when owner removes a nonexistant address
+    it('should allow owner to add a moderator', async () => {
+      const accList = await AccountList.new();
+      await accList.addModerator(modOne);
+      const currMods = await accList.getModerators.call();
+      assert.sameMembers(currMods, [modOne]);
+    });
 
-  test for throw when owner adds an already added moderator
-  test for throw when owner removes a nonexistant moderator
+    it('should allow owner to remove a moderator', async () => {
+      const accList = await AccountList.new([], initialMods);
+      await accList.removeModerator(modOne);
+      const currMods = await accList.getModerators.call();
+      assert.sameMembers(currMods, [modTwo]);
+    });
 
-  test for throw when moderator adds an already added address
-  test for throw when moderator removes a nonexistant address
-*/
+    it('should throw when owner tries to add an existing moderator', async () => {
+      const accList = await AccountList.new([], [modOne]);
+      try { await accList.addModerator(modOne); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    })
+
+    it('should throw when owner tries to remove a nonexistant moderator', async () => {
+      const accList = await AccountList.new();
+      try { await accList.removeModerator(modOne); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    });
+
+    it('should throw when a moderator tries to add a moderator', async () => {
+      const accList = await AccountList.new([], [modOne]);
+      try { await accList.addModerator(modTwo, {from: modOne}); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    });
+
+    it('should throw when a moderator tries to remove a moderator', async () => {
+      const accList = await AccountList.new([], initialMods);
+      try { await accList.removeModerator(modTwo, {from: modOne}); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    });
+  })
+
+  describe('AccountList', async () => {
+    it('should seed with initial accounts', async () => {
+      const accList = await AccountList.new(initialAccounts, initialMods);
+      const currAccounts = await accList.getAccounts.call();
+      assert.sameMembers(currAccounts, initialAccounts);
+    })
+
+    it('should allow owner to add an account', async () => {
+      const accList = await AccountList.new();
+      await accList.addAccount(accOne);
+      const currAccounts = await accList.getAccounts.call();
+      assert.sameMembers(currAccounts, [accOne]);
+    });
+
+    it('should allow owner to remove an account', async () => {
+      const accList = await AccountList.new(initialAccounts, []);
+      await accList.removeAccount(accThree);
+      const currAccounts = await accList.getAccounts.call();
+      assert.sameMembers(currAccounts, [accOne, accTwo]);
+    });
+
+    it('should allow a moderator to add an account', async () => {
+      const accList = await AccountList.new([], initialMods);
+      await accList.addAccount(accOne, {from: modOne});
+      const currAccounts = await accList.getAccounts.call();
+      assert.sameMembers(currAccounts, [accOne]);
+    });
+
+    it('should allow a moderator to remove an account', async () => {
+      const accList = await AccountList.new(initialAccounts, initialMods);
+      await accList.removeAccount(accThree, {from: modOne});
+      const currAccounts = await accList.getAccounts.call();
+      assert.sameMembers(currAccounts, [accOne, accTwo]);
+    });
+
+    it('should throw when owner tries to add an existing account', async () => {
+      const acclist = await AccountList.new(initialAccounts);
+      try { accList.addAccount(accOne); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    })
+
+    it('should throw when owner tries to remove a nonexistant account', async () => {
+      const acclist = await AccountList.new();
+      try { accList.removeAccount(accOne); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    });
+
+    it('should throw when a moderator tries to add an existing account', async () => {
+      const acclist = await AccountList.new(initialAccounts, initialMods);
+      try { accList.addAccount(accOne, {from: modOne}); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    })
+
+    it('should throw when a moderator tries to remove a nonexistant account', async () => {
+      const acclist = await AccountList.new([], initialMods);
+      try { accList.removeAccount(accOne, {from: modOne}); }
+      catch (err) { return true; }
+      throw new Error('I should never see this!');
+    });
+  })
+});
